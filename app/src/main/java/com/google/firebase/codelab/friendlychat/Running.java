471d63b3cbe5e2c58856ed620e6845589ca448f2
= <gigi830720@yahoo.com.tw>
2016-11-23 21:48:18 +0800

    make search and notification simple

---
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/Running.java
@@ -0,0 +1,13 @@
+package com.google.firebase.codelab.friendlychat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Running extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_running);
+    }
+}
