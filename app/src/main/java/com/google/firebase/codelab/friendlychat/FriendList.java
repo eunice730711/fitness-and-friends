88460bf75ea525a6dec998ea2e7101646e856c0f
= <gigi830720@yahoo.com.tw>
2016-12-06 15:08:52 +0800

    correct the friend function

--- app/src/main/java/com/google/firebase/codelab/friendlychat/FriendList.java
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/FriendList.java
@@ -18,18 +18,22 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 //import static com.google.firebase.codelab.friendlychat.R.id.btn_friend;
 
+// 顯示該使用者的好友列表
 public class FriendList extends AppCompatActivity {
     public DatabaseReference mDatabase;
     public UserProfile userProfile;
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLinearLayoutManager;
-    private FirebaseRecyclerAdapter<String,FriendListViewHolder>
+    private FirebaseRecyclerAdapter<Object,FriendListViewHolder>
             mFirebaseAdapter;
 
+    // ViewHolder 可以做每個好友的layout設定
     public static class FriendListViewHolder extends RecyclerView.ViewHolder {
         public TextView txt_friendName;
         public Button btn_deleteFriend;
@@ -48,12 +52,48 @@ public class FriendList extends AppCompatActivity {
                 public void onClick(View v) {
                     // 刪除好友
                     DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-                    // 不知道是個順序
-                    mDatabase.child("Friend").child(userProfile.getUserid()).child("friendid").equalTo(friendid).getRef().removeValue();
-                    mDatabase.child("Friend").child(friendid).child("friendid").equalTo(userProfile.getUserid()).getRef().removeValue();
+                    // 在雙方的好友資料中都需進行刪除
+                    deleteFriend();
                     Toast.makeText(v.getContext(), "恭喜你們已不再是好友", Toast.LENGTH_SHORT).show();
 
                 }
+            });
+        }
+        public void deleteFriend(){
+            DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+            // 從自己的好友資料庫刪除對方
+            mDatabase.child("Friend").child(userProfile.getUserid()).orderByChild("friendid").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        Object o  = dataSnapshot.getValue(Object.class);
+                        String id = ((HashMap<String,String>)o).get("friendid");
+                        // 找到viewholder對應的 requester
+                        if( id.compareTo(friendid)==0) {
+                            dataSnapshot.getRef().removeValue();
+                            break;
+                        }
+                    }
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {}
+            });
+            //從對方的好友資料庫刪除自己
+            mDatabase.child("Friend").child(friendid).orderByChild("friendid").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        Object o  = dataSnapshot.getValue(Object.class);
+                        String id = ((HashMap<String,String>)o).get("friendid");
+                        // 找到viewholder對應的 requester
+                        if( id.compareTo(userProfile.getUserid())==0) {
+                            dataSnapshot.getRef().removeValue();
+                            break;
+                        }
+                    }
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {}
             });
         }
     }
@@ -63,9 +103,12 @@ public class FriendList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_list);
 
+        // 初始化，以及程式與layout的連結
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mRecyclerView = (RecyclerView) findViewById(R.id.friendListRecyclerView);
         mLinearLayoutManager = new LinearLayoutManager(this);
+        // 顯示最新資訊，由上而下
+        mLinearLayoutManager.setReverseLayout(true);
         mLinearLayoutManager.setStackFromEnd(true);
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
         getFriendList();
@@ -77,14 +120,15 @@ public class FriendList extends AppCompatActivity {
     private void getUserProfile() {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         // get the userProfile by instanceId
+        // 該部分須改成以email取得個人資料
         mDatabase.child("UserProfile").orderByChild("instanceid").equalTo(refreshedToken).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     userProfile = dataSnapshot.getValue(UserProfile.class);
                     Log.d("username", userProfile.getUsername());
-                    // 再取得使用者資料後才對資料庫存取
 
+                    // 在取得使用者資料後才對資料庫存取
                     show();
 
                 }
@@ -96,19 +140,22 @@ public class FriendList extends AppCompatActivity {
     private void show(){
         DatabaseReference mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<String,
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Object,
                 FriendListViewHolder>(
-                String.class,
+                Object.class,
                 R.layout.item_friendlist,
                 FriendListViewHolder.class,
-                mFirebaseDatabaseReference.child("Friend").child(userProfile.getUserid())) {
+                mFirebaseDatabaseReference.child("Friend").child(userProfile.getUserid()).orderByChild("friendid")) {
 
             @Override
             protected void populateViewHolder(FriendListViewHolder viewHolder,
-                                              String m, int position) {
-                        viewHolder.txt_friendName.setText(  m);
-                        viewHolder.friendid =   m;
+                                              Object o, int position) {
+
+                        viewHolder.txt_friendName.setText(((HashMap)o).get("friendid").toString());
+                        viewHolder.friendid = ((HashMap)o).get("friendid").toString();
                         viewHolder.userProfile = userProfile;
+
+
             }
         };
         mRecyclerView.setAdapter(mFirebaseAdapter);
